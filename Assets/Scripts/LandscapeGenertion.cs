18f777164a3f44b58ab230e6982b30c43441aa0e
JanoKemp <112688104+JanoKemp@users.noreply.github.com>
2024-07-30 15:23:28 +0200

    added paths

--- Assets/Scripts/LandscapeGenertion.cs
+++ Assets/Scripts/LandscapeGenertion.cs
@@ -4,60 +4,78 @@ using UnityEngine;
 
 public class LandscapeGenertion : MonoBehaviour
 {
-    public GameObject landBlock;
-    public GameObject treeObject;
-    private int landSizeX = 20; //Map size
-    private int landSizeZ = 20; //Map size
-    private int noiseHeight = 3;//determines height of blocks
-    private int gridOffset = 1;//space bewtween each block
-    private float seedX;
-    private float seedY;
-    private List<Vector3> landPos = new List<Vector3>();
+    public GameObject landBlock; //GameObject for individual landBlocks
+    public int gridRadius = 30; //grid radius
+    public float noiseScale = 10f; //scale to adjust frequency of noise. The higher the scale, the smooth the terrain
+    public float heightScale = 5f; //scale to adjust height of of blocks (hills of terrain)
+
+    private GameObject[,] land; //2D array for landBlocks
+
     void Start()
     {
-        seedX = Random.Range(0f, 1000f);
-        seedY = Random.Range(0f, 1000f);
-        for (int x = 0; x < landSizeX;  x++) //increases X size of map until it reaches landSizeX value
+        GenerateTerrain();
+        GeneratePaths();
+    }
+
+    void GenerateTerrain()
+    {
+        land = new GameObject[gridRadius, gridRadius];//initialising 2d arry to hold references for the landBlock gameObjects
+        float rndX = Random.Range(0f, 100f);
+        float rndZ = Random.Range(0f, 100f);//assigning random values for x and Z coordinates so that the perlin noise will generate differently everytime
+                                            //which will result in a unique terrain each time the game is run      
+
+        for (int x = 0; x < gridRadius; x++)
         {
-            for (int z = 0; z < landSizeZ; z++) //increases Z size of map until it reaches landSizeZ value
+            for (int z = 0; z < gridRadius; z++)
             {
-                Vector3 position = new Vector3(x * gridOffset, generatePerlinNoise(x, z, 8f) * noiseHeight, z * gridOffset); //calculating position of where landBlock must be placed
-                
-                GameObject land = Instantiate(landBlock, position, Quaternion.identity) as GameObject;//placing landBlock on position defined above by using instantiate function provided by unity
-                landPos.Add(land.transform.position); //populating landBlock list with positions of instanntiated locks in scene
-                land.transform.SetParent(this.transform);//setting landBlock tansform to transform of object that this script is attached to, aka WorldController
+                float y = Mathf.PerlinNoise((x + rndX) / noiseScale, (z + rndZ) / noiseScale) * heightScale; //using built perlin noise function and multiplaying by heightScale to generate a smooth noise map
+                Vector3 position = new Vector3(x, y, z);// assiging coordinates of each block to postion
+                land[x, z] = Instantiate(landBlock, position, Quaternion.identity);//instantiating each block in the 2d array using the position assigned above
             }
         }
-        ObjectSpawn();//calling method to spawn when game is run
-    }
- 
-    private float generatePerlinNoise (int x, int y,  float noiseScale)
-    {
-        //NoiseScale determines how smooth or rugged the noise is. Higher value = smoother, lower value = more rugged
-        float xNoise = (x + this.transform.position.x + seedX) / noiseScale; //calculates adjusted x coordinate for perlin noise by adding gameobject x val and random seed x value to input x vlaue so that noise is unique
-        float yNoise = (y + this.transform.position.y + seedY) / noiseScale; //calculates adjusted y coordinate adds gameobject y and random seed y value to input y vlaue so that noise is unique
-
-        return Mathf.PerlinNoise(xNoise, yNoise);  //generats perlin noise by using built in perlinNoise function
-    }
-    private Vector3 ObjectSpawnPos()
-    {
-        int rndIndex = Random.Range(0, landPos.Count);//getting random index number in landPos list to spawn objects
-        Vector3 objectPos = new Vector3(
-            landPos[rndIndex].x,
-            landPos[rndIndex].y + 0.5f,
-            landPos[rndIndex].z
-        );//getting position of object in landPos list
-        landPos.RemoveAt(rndIndex);//to prevent overlapping spawns
-        return objectPos;
     }
 
-    private void ObjectSpawn()
+    void GeneratePaths()
     {
-        for (int A = 0; A < 8; A++) 
+        List<Vector2Int> edges = new List<Vector2Int>//Vector2 is used as the terrain is represented as a 2d array
         {
-            GameObject spawnObject = Instantiate(treeObject, ObjectSpawnPos(), Quaternion.identity); //spawns object
-        }
+            new Vector2Int(0, Random.Range(0, gridRadius)),
+            new Vector2Int(gridRadius - 1, Random.Range(0, gridRadius)),
+            new Vector2Int(Random.Range(0, gridRadius), 0),
+            new Vector2Int(Random.Range(0, gridRadius), gridRadius - 1)
+        };// a list to store potential starting points on edges of terrain
+
+        Vector2Int center = new Vector2Int(gridRadius / 2, gridRadius / 2); //store center point of terrain as a end point for enemies to go to
+
+        for (int i = 0; i < 3; i++)
+        {
+            Vector2Int start = edges[Random.Range(0, edges.Count)];//assiging sarting coordinates to start
+            edges.Remove(start);//removing start block
+            CreatePath(start, center);
+        }//randomly choosing 3 starting points
     }
 
-    
+    void CreatePath(Vector2Int start, Vector2Int end)
+    {
+        Vector2Int current = start; //assigning start position generated in GeneratePath function to current variable
+
+        while (current != end) //destroys blocks in generated path until current = end block
+        {
+            Destroy(land[current.x, current.y]);
+            land[current.x, current.y] = null;
+
+            if (current.x != end.x)
+            {
+                current.x += (int)Mathf.Sign(end.x - current.x);//Mathf.Sign return 1, -1, 0. If end.x > current.x it  will move right, if < then left
+            }
+            else if (current.y != end.y)
+            {
+                current.y += (int)Mathf.Sign(end.y - current.y);//same as above but will determine if y(vertical) should move up or down
+            }
+        }
+
+        // Destroy the final block at the center
+        Destroy(land[end.x, end.y]);
+        land[end.x, end.y] = null;
+    }
 }
