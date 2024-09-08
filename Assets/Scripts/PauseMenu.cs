9d72bde2082eeb9204478c57cf32fd0494c2bb6d
JanoKemp <112688104+JanoKemp@users.noreply.github.com>
2024-09-08 16:46:21 +0200

    Added pause button

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -17,14 +17,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (isPaused)
-            {
-                Resume();
-            }
-            else Pause();
-        }
+        
     }
     public void Pause()
     {
